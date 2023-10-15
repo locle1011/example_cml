196e0806384b86025fae8121625cc99d6c16afb5
loc.1011 <loc.le1011@hcmut.edu.vn>
2023-10-15 13:37:59 +0700

    reduce number of estimators

--- train.py
+++ train.py
@@ -13,7 +13,8 @@ y_test = np.genfromtxt("data/test_labels.csv")
 
 # Fit a model
 depth = 6
-clf = RandomForestClassifier(max_depth=depth)
+n_estimators = 10
+clf = RandomForestClassifier(max_depth=depth, n_estimators=n_estimators)
 clf.fit(X_train, y_train)
 
 acc = clf.score(X_test, y_test)
